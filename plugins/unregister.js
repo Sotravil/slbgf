d00d514d8c2c19cbde65cfa300e77160082a0ad6
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-25 18:37:30 -0500

    Update unregister.js

--- plugins/unregister.js
+++ plugins/unregister.js
@@ -3,9 +3,9 @@ let handler = async function (m, { args }) {
   if (!args[0]) throw 'Serial Number kosong'
   let user = global.DATABASE._data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw 'Serial Number salah'
+  if (args[0] !== sn) throw 'Número de serie incorrecto'
   user.registered = false
-  m.reply(`Unreg berhasil!`)
+  m.reply(`se desregistro correctamente!`)
 }
 handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <SN|SERIAL NUMBER>')
 handler.tags = ['exp']
