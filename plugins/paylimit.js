e238dfbf6ed4d92f49a12b3b71223cb4da518e3f
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-25 18:44:37 -0500

    Update paylimit.js

--- plugins/paylimit.js
+++ plugins/paylimit.js
@@ -1,20 +1,20 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let pajak = 0.02
 let handler = async (m, { conn, text }) => {
-    if (!text) throw 'Masukkan jumlah Limit yang akan diberi'
+    if (!text) throw 'Ingrese el número de límites que se darán'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
     if (!who) throw 'Etiqueta a alguien'
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (isNaN(txt)) throw 'Hanya angka'
+    if (isNaN(txt)) throw 'Solo números'
     let poin = parseInt(txt)
     let limit = poin
     let pjk = Math.ceil(poin * pajak)
     limit += pjk
     if (limit < 1) throw 'Minimal 1'
     let users = global.DATABASE._data.users
-    if (limit > users[m.sender].limit) throw 'Limit tidak mencukupi untuk mentransfer'
+    if (limit > users[m.sender].limit) throw 'Límites insuficientes para la transferencia'
     users[m.sender].limit -= limit
     users[who].limit += poin
 
