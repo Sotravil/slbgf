44526d8ba8b39b715477f383ca0efee8c5b4ebb8
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-30 10:20:38 -0500

    Update unbanuser.js

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -1,13 +1,13 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text}) => {
-    if (!text) throw 'Siapa yang mau di unbanned?'
+    if (!text) throw '¿Quién quiere ser desbaneado? ?'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Tag salah satu lah'
+    if (!who) throw 'Etiqueta a alguien'
     let users = global.DATABASE._data.users
     users[who].banned = false
-    conn.reply(m.chat, `berhasil unbanned`, m)
+    conn.reply(m.chat, `desbaneado con éxito`, m)
 }
 handler.help = ['ban']
 handler.tags = ['owner']
