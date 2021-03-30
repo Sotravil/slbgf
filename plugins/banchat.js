cd1b228e59339b13af4c942952a6258f31356b81
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-30 10:23:55 -0500

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, participants }) => {
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].isBanned = true
     m.reply('Done!')
-  // } else m.reply('Ada nomor host disini...')
+  // } else m.reply('Aquí hay un número de host...')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
