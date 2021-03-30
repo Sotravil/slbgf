e0b9e051d0772fe376e669b4b0760f1774281048
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-30 11:36:18 -0500

    Update addprems.js

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
 let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw 'Tag salah satu lah,dan masukkan nomor untuk di verivikasi !'
+  if (!who) throw 'Etiquete uno o ingrese el número para verificación !'
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].premium = true
   var nomor = m.sender
