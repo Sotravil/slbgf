1f8c635dee5ae83070fdf75e3b7ddfe9d262269f
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-31 12:00:34 -0500

    Update setbye.js

--- plugins/setbye.js
+++ plugins/setbye.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     if (isROwner) global.conn.bye = text
     else if (isOwner) conn.bye = text
     else global.DATABASE._data.chats.sBye = text
-    m.reply('Bye berhasil diatur\n@user (Mention)')
-  } else throw 'Teksnya mana?'
+    m.reply('Bye se configuro correctamente\n@user (Mention)')
+  } else throw 'y el texto?'
 }
-handler.help = ['setbye <teks>']
+handler.help = ['setbye <texto>']
 handler.tags = ['owner', 'group']
 
 handler.command = /^setbye$/i
