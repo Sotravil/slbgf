60a0538c7ea814253f2f0df2a5047a4ed20a8e77
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-31 20:55:59 -0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -3,18 +3,18 @@ let yts = require('yt-search')
 let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
-  if (!text) throw 'Cari apa?'
+  if (!text) throw 'Qué estás buscando?'
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
+  if (!vid) throw 'Video/Audio Extraviado'
   let isVideo = /2$/.test(command)
   let { dl_link, thumb, title, filesize, filesizeF} = await (isVideo ? ytv : yta)(vid.url, 'id4')
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
+*Titulo:* ${title}
+*Tamaño:* ${filesizeF}
 *Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+*${isLimit ? 'Póntelo': ''}Link:* ${dl_link}
 `.trim(), m)
   let _thumb = {}
   try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
@@ -25,7 +25,7 @@ let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
 *Source:* ${vid.url}
 `.trim(), m, false, _thumb || {})
 }
-handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
+handler.help = ['play', 'play2'].map(v => v + ' <búsqueda>')
 handler.tags = ['downloader']
 handler.command = /^play2?$/i
 
