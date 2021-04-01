a1ce95ab1ee4e2e35d3294e5a0b9fbefda8485d1
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-31 20:45:22 -0500

    Update buylimit.js

--- plugins/buylimit.js
+++ plugins/buylimit.js
@@ -7,9 +7,9 @@ let handler = async (m, { conn, command, args }) => {
     global.DATABASE._data.users[m.sender].exp -= xpperlimit * count
     global.DATABASE._data.users[m.sender].limit += count
     conn.reply(m.chat, `-${xpperlimit * count} XP\n+ ${count} Limit`, m)
-  } else conn.reply(m.chat, `XP no es suficiente para comprar  ${count} limit`, m)
+  } else conn.reply(m.chat, `XP no es suficiente para comprar  ${count} coins`, m)
 }
-handler.help = ['buy<jumlah limit>', 'buy <jumlah limit>', 'buyall']
+handler.help = ['buy<numero de coins>', 'buy <numero de coins>', 'buyall']
 handler.tags = ['xp']
 handler.command = /^buy([0-9]+)|buy|buyall$/i
 handler.owner = false
