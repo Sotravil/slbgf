6516d916c6fbfe6f15677879d2c5d420a373e665
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-03-31 20:48:41 -0500

    Update calc.js

--- plugins/calc.js
+++ plugins/calc.js
@@ -27,7 +27,7 @@ let handler = async (m, { conn, text }) => {
     m.reply(`*${format}* = _${result}_`)
   } catch (e) {
     if (e == undefined) throw 'Isinya?'
-    throw 'Format salah, hanya 0-9 dan Simbol -, +, *, /, ×, ÷, π, e, (, ) yang disupport'
+    throw 'Formato incorrecto, solo 0-9 y símbolo -, +, *, /, ×, ÷, π, e, (, ) que es compatible'
   }
 }
 handler.help = ['calc <expression>']
