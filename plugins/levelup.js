11f8688393dc2f1d713d8f268f61773a47534e70
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-04-02 08:58:33 -0500

    Update levelup.js

--- plugins/levelup.js
+++ plugins/levelup.js
@@ -5,8 +5,8 @@ let handler = m => {
   if (!levelling.canLevelUp(user.level, user.exp, global.multiplier)) {
     let { min, xp, max } = levelling.xpRange(user.level, global.multiplier)
     throw [`
-Level *${user.level} (${user.exp - min}/${xp})*                            Te faltan *$max - user.exp* para subir de nivel!
-`.trim(), {...user, min, xp, max}]
+Level *${user.level} (${user.exp - min}/${xp})*                            Te faltan *${max - user.exp}* para subir de nivel!
+`.trim(), ...user, min, xp, max
   }
   user.level++
   m.reply(`
